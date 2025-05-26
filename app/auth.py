510d0fac4978480cdfe63753c398e79acc66fee7
vvvver <158335958+vvvver@users.noreply.github.com>
2025-05-26 18:28:53 +0300

    Update auth.py

--- app/auth.py
+++ app/auth.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, render_template, request, redirect, url_for, flash
 from flask_login import login_user, logout_user, login_required, current_user
-from models import User
+from .models import User
 
 auth_bp = Blueprint('auth', __name__, template_folder='templates', url_prefix='/auth')
 
