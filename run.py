0f3b80c9a25da2945361f989bdebff104b3f8965
vvvver <158335958+vvvver@users.noreply.github.com>
2025-05-26 18:44:07 +0300

    Update run.py

--- run.py
+++ run.py
@@ -1,6 +1,11 @@
 from app.app import create_app
+from app.models import db
 import os
 app = create_app()
+
+with app.app_context():
+    db.create_all()
+    
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
